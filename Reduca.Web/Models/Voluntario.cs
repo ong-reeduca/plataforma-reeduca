ce57bde696a971183d0cba09d4c40545646bcf76
Jéssica Nathany <jessicanathany.web@gmail.com>
2019-03-17 20:37:55 -0300

    criação do proejto Reduca.Api backend e criação do projeto web frontend, onde sera feito a parte de apresentação ao usuário

---
+++ Reduca.Web/Models/Voluntario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reduca.Web.Models
+{
+    public class Voluntario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
