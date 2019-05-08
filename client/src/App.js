ae7d5525d8fdbbb7bc82284ef757643d29e7cf73
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-05-08 10:20:02 -0300

    setado inicio do projeto

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>WebApp Reduca</h3>
+    </div>
+  );
+}
+
+export default App;
